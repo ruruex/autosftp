c6668e5600387a849559749ac82b0122bc3b56bd
zxie <ruruex@163.com>
2021-08-15 15:06:26 +0800

    Add the average speed function

--- sftpup.py
+++ sftpup.py
@@ -2,8 +2,8 @@ from json.decoder import JSONDecodeError
 import os
 import argparse
 import json
-#import time
 import datetime
+import syslog
 
 import pysftp
 
@@ -20,12 +20,18 @@ def sftp_connect(hostname,sftp_username,sftp_password,home_dir,sftp_action,sftp_
             else:
                 return 'wrong sftp_action'
 
-def write_log(content,logfile_name):
+def write_log(basic_content, content,logfile_name):
     '''
-    Write log to file
+    Write log to file and system syslog
     '''
     with open (logfile_name,'a') as logfileobj:
         logfileobj.write(content)
+    
+    syslog.openlog(ident='sftpup.py',facility=syslog.LOG_LOCAL0)
+    try:
+        syslog.syslog(syslog.LOG_NOTICE,basic_content)
+    except TypeError:
+        raise Exception(basic_content)
 
 def Main():
     '''
@@ -43,6 +49,7 @@ def Main():
 
     if os.path.isfile(args.filename):
         sftp_file = args.filename
+        file_size = round(os.path.getsize(sftp_file)/1024/1024,2) # in MBytes
     else:
         os._exit('file does not exits')
 
@@ -58,14 +65,19 @@ def Main():
     sftp_connect(config_dict['hostname'],config_dict['username'],config_dict['password'],config_dict['home_dir'],sftp_action,sftp_file)
     endtime = datetime.datetime.now()
     duration = endtime - starttime
-    print(f'The {sftp_action} takes: {duration}')
+    #print(f'The {sftp_action} takes: {duration}')
 
-    log_time = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S ")
+    # Calc avgerage throughput
+    avg_speed = round(file_size*8/duration.total_seconds(),2)
+
+    log_time = datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
     machine_timezone = datetime.datetime.now(datetime.timezone.utc).astimezone().tzname()
     #print(f'Local time is {local_time}')
 
-    log_str = f"{log_time} {machine_timezone} sftpup.py: {sftp_action} to {config_dict['hostname']} {config_dict['home_dir']} takes: {duration}  \n"
-    write_log(log_str,'sftp.log')
+    # basic_log_str for syslog, log_str for log file
+    basic_log_str = f"{config_dict['hostname']}{config_dict['home_dir']} {sftp_action} {sftp_file} size:{file_size}MBytes takes: {duration} average throughput is {avg_speed}mbps\n"
+    log_str = f"{log_time} {machine_timezone} sftpup.py: " + basic_log_str
+    write_log(basic_log_str,log_str,'sftp.log',)
 
     
 if __name__ == '__main__':
